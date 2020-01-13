c3c85a052bd2cbbe12e7ee9dbbd03dc3d48f973c
bas1to <52505991+bas1to@users.noreply.github.com>
2020-01-13 20:14:51 +0100

    python testfiles TCP send/receive

---
+++ Client.py
@@ -0,0 +1,33 @@
+import socket  # Import socket module
+import os
+import re
+
+
+if __name__ == "__main__":
+    s = socket.socket()  # Create a socket object
+    port = 50000  # Reserve a port for your service every new transfer wants a new port or you must wait.
+
+    s.connect(('localhost', port))
+    x = 0
+
+    st = str(x)
+    byt = st.encode()
+    s.send(byt)
+
+    # send message for hundred times
+    while x < 100:
+        st = str(x)
+        byt = st.encode()
+        s.send(byt)
+
+        print(x)
+
+        while True:
+            data = s.recv(1024)
+            if data:
+                print(data)
+                x += 1
+                break
+
+            else:
+                print('no data received')
