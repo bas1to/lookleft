7e548bb27e5a77b6deeb3dc9c4ff167b0282d86f
bas1to <basitob@gmail.com>
2020-01-17 15:26:46 +0100

    pubsub updated

--- PubSub.js
+++ PubSub.js
@@ -15,44 +15,20 @@ mongo.MongoClient.connect (mongodbUri, function (err, db) {
       // import express JS module into app 
 
 // START CHILD PROCESS CODE
-// and creates its variable. 
-var express = require('express'); 
-var app = express(); 
+var util = require("util");
 
-// Creates a server which runs on port 3000 and 
-// can be accessed through localhost:3000 
-app.listen(3000, function() { 
-	console.log('server running on port 3000'); 
-} ) 
+var spawn = require("child_process").spawn;
+var process = spawn('python',["Energy.py"]);
 
-// Function callName() is executed whenever 
-// url is of the form localhost:3000/name 
-app.get('/name', callName); 
+util.log('readingin')
 
-function callName(req, res) { 
-	
-	// Use child_process.spawn method from 
-	// child_process module and assign it 
-	// to variable spawn 
-	var spawn = require("child_process").spawn; 
-	
-	// Parameters passed in spawn - 
-	// 1. type_of_script 
-	// 2. list containing Path of the script 
-	// and arguments for the script 
-	
-	// E.g : http://localhost:3000/name?firstname=Mike&lastname=Will 
-	// so, first name = Mike and last name = Will 
-	var process = spawn('python',["./Energy.py", 
-							req.query.firstname, 
-							req.query.lastname] ); 
+process.stdout.on('data',function(chunk){
 
-	// Takes stdout data from script which executed 
-	// with arguments and send this data to res object 
-	process.stdout.on('data', function(data) { 
-		res.send(data.toString()); 
-	} ) 
- } // END OF CODE SNIPPET 
+    var textChunk = chunk.toString('utf8');// buffer to string
+
+    util.log(textChunk);
+});
+// END OF CODE SNIPPET 
 
 
 
